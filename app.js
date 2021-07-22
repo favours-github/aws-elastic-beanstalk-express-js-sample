a39043c46b93bdc8b3669cb6bbbc9a6ca4a68e93
fnwosu <favourndudinwosu@gmail.com>
2021-07-22 21:11:15 +0100

    full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello cloud elite to the world'));
+app.get('/', (req, res) => res.send('I am an instructor for cloudelite.us it's my pleasure to meet you all'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
